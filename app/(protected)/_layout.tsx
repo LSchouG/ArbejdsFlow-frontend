681a6f2e629f522d73a13f0578f6b7a044540d2f
Lars S G <larsgregersen1990@gmail.com>
2026-02-17 14:17:38 +0100

    Changing design of the layout

---
+++ app/(protected)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot, Redirect } from "expo-router";
+import { useAuth } from "../../providers/AuthProvider";
+
+export default function ProtectedLayout() {
+  const { user, loading } = useAuth();
+
+  if (loading) return null;
+
+  if (!user) return <Redirect href="/login" />;
+
+  return <Slot />;
+}
